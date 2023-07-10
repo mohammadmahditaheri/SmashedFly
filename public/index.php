35c60f805c87d0ba69a83fa19b16ea1b7a4049f4
Mohammad Mahdi Taheri <mmtaheri@yandex.com>
2023-07-10 23:44:38 +0400

    -+ clean up and reformatting

--- public/index.php
+++ public/index.php
@@ -10,9 +10,9 @@ require_once BASE_PATH . '/vendor/autoload.php';
 // request is received
 $request = Request::createFromGlobals();
 
-// perform some login
-$kernel = Kernel::make();
-$response = $kernel->handle($request);
+// perform some logic
+$response = (Kernel::make())
+    ->handle($request);
 
 // send back the response
 $response->send();
