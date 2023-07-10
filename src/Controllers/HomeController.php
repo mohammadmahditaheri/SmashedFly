cd43e3946fff8ddc4368b7ed9b8492baaa1894d6
Mohammad Mahdi Taheri <mmtaheri@yandex.com>
2023-07-10 23:44:07 +0400

    -+ inherit from parent controller -+  use the ready response

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -2,14 +2,15 @@
 
 namespace App\Controllers;
 
+use MohammadMahdi\Framework\Controllers\BaseController;
 use MohammadMahdi\Framework\Http\Response;
 
-class HomeController
+class HomeController extends BaseController
 {
     public function index(): Response
     {
         $content = '<h1>Hello from index method of the HomeController</h1>';
 
-        return (new Response($content));
+        return $this->response($content);
     }
 }
